e96340fdd756df518cc8a30f6206d3df67736ed3
Yun Seongmin <ilcm96@gmail.com>
2024-09-17 17:19:06 +0900

    refactor: split swagger url into local and prod

--- src/main/java/tourapi24/backend/config/SwaggerConfig.java
+++ src/main/java/tourapi24/backend/config/SwaggerConfig.java
@@ -2,15 +2,30 @@ package tourapi24.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    @Profile("prod")
+    public OpenAPI prodOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://tourapi.seongmin.dev"))
+                .info(new Info()
+                        .title("Tour API")
+                        .description("Tour API Documentation")
+                        .version("v0.1.0"));
+    }
+
+    @Bean
+    @Profile("local")
+    public OpenAPI localOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8080"))
                 .info(new Info()
                         .title("Tour API")
                         .description("Tour API Documentation")
