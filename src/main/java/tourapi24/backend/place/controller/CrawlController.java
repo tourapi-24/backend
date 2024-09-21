c1a12ebeb42ec590f113d0ae459cf2d753aa47b2
Yun Seongmin <ilcm96@gmail.com>
2024-09-21 21:24:56 +0900

    fix: CrawlController에 적절한 어노테이션 추가

--- src/main/java/tourapi24/backend/place/controller/CrawlController.java
+++ src/main/java/tourapi24/backend/place/controller/CrawlController.java
@@ -5,11 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tourapi24.backend.place.service.crawl.Crawl;
 
-@RestController("/crawl")
+@RestController
+@RequestMapping("/crawl")
 @RequiredArgsConstructor
 @Hidden
 public class CrawlController {
