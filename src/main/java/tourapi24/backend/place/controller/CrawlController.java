6102e93c2b668b6946229ed9947bc7b0196d00d4
Yun Seongmin <ilcm96@gmail.com>
2024-09-21 21:15:27 +0900

    feat: 크롤링 테스트용 임시 엔드포인트 추가

---
+++ src/main/java/tourapi24/backend/place/controller/CrawlController.java
@@ -0,0 +1,31 @@
+package tourapi24.backend.place.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tourapi24.backend.place.service.crawl.Crawl;
+
+@RestController("/crawl")
+@RequiredArgsConstructor
+@Hidden
+public class CrawlController {
+
+    private final Crawl crawl;
+
+    @Value("${key.chatbot}")
+    private String key;
+
+    //TODO: 크롤링 테스트용 코드
+    @GetMapping
+    public ResponseEntity<?> crawl(@RequestParam String key) {
+        if (!this.key.equals(key)) {
+            return ResponseEntity.badRequest().build();
+        }
+        crawl.crawlAndSave(3000);
+        return ResponseEntity.ok().build();
+    }
+}
