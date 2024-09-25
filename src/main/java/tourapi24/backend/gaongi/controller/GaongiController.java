25ba200201737dd247b994bb9ed0ab3757047dcd
Yun Seongmin <ilcm96@gmail.com>
2024-09-25 18:00:44 +0900

    doc: 가옹이 관련 swagger 작성

--- src/main/java/tourapi24/backend/gaongi/controller/GaongiController.java
+++ src/main/java/tourapi24/backend/gaongi/controller/GaongiController.java
@@ -1,6 +1,11 @@
 package tourapi24.backend.gaongi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +21,25 @@ import tourapi24.backend.gaongi.service.GaongiService;
 @RestController
 @RequestMapping("/gaongi")
 @RequiredArgsConstructor
+@Tag(name = "Gaongi")
 public class GaongiController {
 
     private final GaongiService gaongiService;
 
     @GetMapping
+    @Operation(
+            summary = "가옹이 정보를 조회합니다",
+            description = "가옹이 레벨은 1~5까지 아기가옹이부터 황금가옹이까지 입니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "가옹이 정보 조회 성공",
+                            content = @Content(
+                                    schema = @Schema(implementation = GaongiResponse.class)
+                            )
+                    ),
+            }
+    )
     public ResponseEntity<GaongiResponse> getGaongi(
             @Parameter(hidden = true) @CurrentUser CurrentUserInfo userInfo
     ) {
@@ -31,6 +50,18 @@ public class GaongiController {
     }
 
     @PostMapping("/exp/increase")
+    @Operation(
+            summary = "경험치를 1 증가시킵니다",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "경험치 증가 성공",
+                            content = @Content(
+                                    schema = @Schema(implementation = GaongiResponse.class)
+                            )
+                    ),
+            }
+    )
     public ResponseEntity<GaongiResponse> increaseExp(
             @Parameter(hidden = true) @CurrentUser CurrentUserInfo userInfo
     ) {
